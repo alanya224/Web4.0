8c9fe55e526df440ba1c3cdb6e227d1a61df8d11
alanya224 <alanya224@gmail.com>
2017-01-08 03:50:59 +0700

    7/1/2016

---
+++ front-end/TechKidsHotGirl/js/main.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+
+  var itemTemplate = Handlebars.compile($("#item-template").html());
+
+   $.ajax({  // $ == jquery
+    type : "get",
+    url  : "imagesData.json"
+  }).then(function(data){
+    var itemHtml = itemTemplate(data);
+    $("#item_list").html(itemHtml);
+    $("#item_list").masonry({
+      itemSelector: ".item_container",
+      columnWidth: ".item_container",
+      percentPosition: true
+    });
+  }).fail(function(error){
+    console.log("Error: " + error);
+  });
+
+});
