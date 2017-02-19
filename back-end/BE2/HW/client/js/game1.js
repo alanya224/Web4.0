fb48c661dc4151e053336312edcb55b182be0092
alanya224 <alanya224@gmail.com>
2017-02-19 10:46:58 +0700

    homework 2

---
+++ back-end/BE2/HW/client/js/game1.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+    $(window).on('scroll', function () {
+        if ($(window).scrollTop() == 0) {
+            $('.navbar-fixed-top').css("background-color", "transparent");
+        } else {
+            $('.navbar-fixed-top').css("background-color", "#141319");
+        }
+    });
+    var game = $('canvas:first');
+    game.remove();
+
+});
---
+++ back-end/BE2/HW/gulpfile.js
@@ -0,0 +1,30 @@
+var csslint = require('gulp-csslint')
+var jshint = require('gulp-jshint')
+var nodemon = require('gulp-nodemon')
+var gulp = require('gulp')
+
+//gulp.task('default',['task-1','task-2']);
+gulp.task('task-1', () =>
+    gulp.src(['client/js/*.js'])
+        .pipe(jshint())
+        .pipe(jshint.reporter('jshint-stylish'))
+);
+gulp.task('task-2', function() {
+  gulp.src('client/css/*.css')
+    .pipe(csslint())
+    .pipe(csslint.formatter());
+});
+gulp.task('develop', function () {
+  var stream = nodemon({ script: 'app.js'
+          , ext: 'css js'
+          , tasks: ['task-1','task-2'] })
+
+  stream
+      .on('restart', function () {
+        console.log('restarted!')
+      })
+      .on('crash', function() {
+        console.error('Application has crashed!\n')
+         stream.emit('restart', 10)  // restart the server in 10 seconds
+      })
+})
